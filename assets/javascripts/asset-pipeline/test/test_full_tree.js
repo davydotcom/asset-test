df93504dc3fd5a4afc219ae7b348eaad91b624a2
David Estes <davydotcom@gmail.com>
2014-11-09 00:07:18 -0500

    few more test assets

--- assets/javascripts/asset-pipeline/test/test_full_tree.js
+++ assets/javascripts/asset-pipeline/test/test_full_tree.js
@@ -2,3 +2,16 @@
 //=require_full_tree libs
 
 console.log("Initial Testing Stack for dependency load order");
+
+var test = function() {
+	var myVar = 0
+	var myArr = [];
+	for(myVar = 0; myVar < 1000; myVar++) {
+		if(myVar %2) {
+			myArr.push(myVar);
+		}
+	}
+}
+test();
+
+alert("This is me changing a js file in my asset pipeline app");
